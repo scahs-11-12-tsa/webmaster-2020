9e04be9d9eed8195e9f7d7dee9c404725ad2ca65
Albert Zhang <alchzh@gmail.com>
2020-01-15 14:50:15 +0000

    animate underline

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,13 +10,13 @@
         </b-navbar-item>
     </template>
     <template slot="start">
-        <b-navbar-item href="#">
+        <b-navbar-item href="#" class="anim-under">
             About
         </b-navbar-item>
-        <b-navbar-item tag="router-link" :to="{ path: '/cars' }">
+        <b-navbar-item tag="router-link" :to="{ path: '/cars' }" class="anim-under">
             Cars
         </b-navbar-item>
-        <b-navbar-item href="#">
+        <b-navbar-item href="#" class="anim-under">
             Impact
         </b-navbar-item>
         <b-navbar-item class="logo force-center" href="/">
@@ -27,13 +27,13 @@
           >
           raijin
         </b-navbar-item>
-        <b-navbar-item href="#">
+        <b-navbar-item href="#" class="anim-under">
             News
         </b-navbar-item>
-        <b-navbar-item href="#">
+        <b-navbar-item href="#" class="anim-under">
             Shop
         </b-navbar-item>
-        <b-navbar-item href="#">
+        <b-navbar-item href="#" class="anim-under">
             Contact
         </b-navbar-item>
     </template>
